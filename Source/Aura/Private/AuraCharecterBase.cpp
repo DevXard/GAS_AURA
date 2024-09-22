82ce85d473b63a38ad1ea3e68947146cbadadd2f
Pavlin <dev.xard@gmail.com>
2024-09-22 00:32:19 -0400

    Created charecter mesh and socket for weapon + creating aura and goblin chars

--- Source/Aura/Private/AuraCharecterBase.cpp
+++ Source/Aura/Private/AuraCharecterBase.cpp
@@ -7,7 +7,9 @@
 AAuraCharecterBase::AAuraCharecterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
-
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AAuraCharecterBase::BeginPlay()
